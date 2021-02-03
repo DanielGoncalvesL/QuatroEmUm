cceab5fcb790eba2affb4f64df0ed725650cd97c
Daniel Gonçalves <danielopesgl05@gmail.com>
2021-02-03 12:39:55 -0300

    Criação das Pages dos quatro aplicativos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,26 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'calculadora',
+    loadChildren: () => import('./pages/calculadora/calculadora.module').then( m => m.CalculadoraPageModule)
+  },
+  {
+    path: 'conversor-temperatura',
+    loadChildren: () => import('./pages/conversor-temperatura/conversor-temperatura.module').then( m => m.ConversorTemperaturaPageModule)
+  },
+  {
+    path: 'verificar-numero',
+    loadChildren: () => import('./pages/verificar-numero/verificar-numero.module').then( m => m.VerificarNumeroPageModule)
+  },
+  {
+    path: 'calculadora-idade',
+    loadChildren: () => import('./pages/calculadora-idade/calculadora-idade.module').then( m => m.CalculadoraIdadePageModule)
   }
 ];
 
---
+++ src/app/pages/verificar-numero/verificar-numero.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verificar-numero',
+  templateUrl: './verificar-numero.page.html',
+  styleUrls: ['./verificar-numero.page.scss'],
+})
+export class VerificarNumeroPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
